b7ce1482c3c8384adb2b36b713ee1717466eece6
mathe <mathewlogan90@gmail.com>
2025-09-29 15:33:40 -0400

    Made test_file.py just a tiny bit better

--- brispy/singlefile/tests/test_file.py
+++ brispy/singlefile/tests/test_file.py
@@ -4,15 +4,14 @@
 from unittest import TestCase
 
 from ..file import SingleFile
-from ..row import SingleFileRow
 
 
 class TestSingleFile(TestCase):
 
-    TEST_PATH = 'C:\\Users\\mathe\\OneDrive\\Documents\\horses\\gulfstream\\pps\\data\\2025\\GPX0912.drf'
+    TEST_PATH = 'C:\\Users\\mathe\\OneDrive\\Documents\\horses\\gulfstream\\pps\\2025\\data\\GPX0912.drf'
 
     def test_create(self):
         sf = SingleFile.create(self.TEST_PATH)
-        if sf.rows:
-            row: SingleFileRow = sf.rows[0]
-            self.assertEqual(row.track, 'GP')
+        self.assertIsInstance(sf, SingleFile)
+        self.assertIsNotNone(sf.rows)
+        self.assertIsInstance(sf.rows, list)
